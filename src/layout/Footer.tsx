d7b80f59da349f40cc6a5c9c28be734f68df682d
Matias Arancibia <mbenjaminarancibia@gmail.com>
2025-09-29 14:35:46 -0300

    navbar and footer

---
+++ src/layout/Footer.tsx
@@ -0,0 +1,28 @@
+function Footer() {
+    return (
+        <>
+            <div className="mt-3 pb-3 pt-3 bg-dark text-light text-center text-small">
+                <p className="mb-1">&copy; 2025 LevelUp</p>
+                <ul className="list-inline">
+                    <li className="list-inline-item">
+                        <a href="#" className="footer-link">
+                            Instagram
+                        </a>
+                    </li>
+                    <li className="list-inline-item">
+                        <a href="#" className="footer-link">
+                            Twitter
+                        </a>
+                    </li>
+                    <li className="list-inline-item">
+                        <a href="#" className="footer-link">
+                            Support
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </>
+    );
+}
+
+export default Footer;
---
+++ src/layout/Navbar.tsx
@@ -0,0 +1,119 @@
+import { Link } from "react-router-dom";
+
+function Navbar() {
+    return (
+        <>
+            <div className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+                <div className="container">
+                    <Link className="navbar-brand" to="/">
+                        LevelUp
+                    </Link>
+                    <button
+                        className="navbar-toggler"
+                        type="button"
+                        data-bs-toggle="collapse"
+                        data-bs-target="#navbarNavDropdown"
+                        aria-controls="navbarNavDropdown"
+                        aria-expanded="false"
+                        aria-label="Toggle navigation"
+                    >
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div
+                        className="collapse navbar-collapse"
+                        id="navbarNavDropdown"
+                    >
+                        <div className="d-flex align-items-center mx-auto w-100">
+                            <ul className="navbar-nav ">
+                                <li className="nav-item dropdown">
+                                    <Link
+                                        className="nav-link btn btn-nav btn-ripple-water"
+                                        to="/products"
+                                        id="navbarDropdown"
+                                        role="button"
+                                        aria-expanded="false"
+                                    >
+                                        Productos
+                                    </Link>
+                                    <ul
+                                        className="dropdown-menu"
+                                        aria-labelledby="navbarDropdown"
+                                    >
+                                        <li>
+                                            <Link
+                                                className="dropdown-item"
+                                                to="/products"
+                                            >
+                                                Magic: The Gathering
+                                            </Link>
+                                        </li>
+                                        <li>
+                                            <Link
+                                                className="dropdown-item"
+                                                to="/"
+                                            >
+                                                Accesorios
+                                            </Link>
+                                        </li>
+                                    </ul>
+                                </li>
+                            </ul>
+                            <form className="d-flex position-relative mx-auto w-lg-auto my-2 my-lg-0">
+                                <span className="position-absolute top-50 start-0 translate-middle-y ps-3">
+                                    🔍
+                                </span>
+                                <input
+                                    className="form-control ps-5 rounded-pill"
+                                    type="search"
+                                    placeholder="Buscar..."
+                                    aria-label="Buscar"
+                                ></input>
+                            </form>
+
+                            <div className="navbar-nav"></div>
+                            <li className="nav-item dropdown">
+                                <button
+                                    className="nav-link btn btn-nav btn-ripple-water"
+                                    aria-expanded="false"
+                                >
+                                    <i className="bi bi-person"></i> Cuenta
+                                </button>
+                                <ul
+                                    className="dropdown-menu"
+                                    aria-labelledby="navbarNavDropdown"
+                                >
+                                    <li>
+                                        <Link
+                                            className="dropdown-item"
+                                            to="/login"
+                                        >
+                                            Iniciar Sesión
+                                        </Link>
+                                    </li>
+                                    <li>
+                                        <Link
+                                            className="dropdown-item"
+                                            to="/register"
+                                        >
+                                            Registro
+                                        </Link>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li>
+                                <a
+                                    className="nav-link btn btn-nav btn-ripple-water"
+                                    href="checkout.html"
+                                    aria-expanded="false"
+                                >
+                                    🛒 Carrito
+                                </a>
+                            </li>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
+export default Navbar;
