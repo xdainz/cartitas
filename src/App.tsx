1b9e598c6c49c01fd8576c3ee55986a1b5c367b9
Matias Arancibia <mbenjaminarancibia@gmail.com>
2025-10-06 10:20:30 -0300

    card rework

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import "../src/assets/css/color-palette.css";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Products from "./pages/Products";
