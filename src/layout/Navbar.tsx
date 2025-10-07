a65d800c84e191131f76e63cb9e44501ce6649ed
Matias Arancibia <mbenjaminarancibia@gmail.com>
2025-10-06 23:04:48 -0300

    base laayout navbar

--- src/layout/Navbar.tsx
+++ src/layout/Navbar.tsx
@@ -1,111 +1,47 @@
+import Button from "../components/Button";
 import { Link } from "react-router-dom";
 
 function Navbar() {
     return (
-        <>
-            <div className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-                <div className="container">
-                    <Link className="navbar-brand" to="/">
-                        LevelUp
-                    </Link>
-                    <button
-                        className="navbar-toggler"
-                        type="button"
-                        data-bs-toggle="collapse"
-                        data-bs-target="#navbarNavDropdown"
-                        aria-controls="navbarNavDropdown"
-                        aria-expanded="false"
-                        aria-label="Toggle navigation"
-                    >
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div
-                        className="collapse navbar-collapse"
-                        id="navbarNavDropdown"
-                    >
-                        <div className="d-flex align-items-center mx-auto w-100">
-                            <ul className="navbar-nav ">
-                                <li className="nav-item dropdown">
-                                    <Link
-                                        className="nav-link btn btn-nav btn-ripple-water"
-                                        to="/Productos"
-                                    >
-                                        Productos
-                                    </Link>
-                                    <ul
-                                        className="dropdown-menu"
-                                        aria-labelledby="navbarDropdown"
-                                    >
-                                        <li>
-                                            <Link
-                                                className="dropdown-item"
-                                                to="/Productos/TCG"
-                                            >
-                                                Trading Card Games
-                                            </Link>
-                                        </li>
-                                        <li>
-                                            <Link
-                                                className="dropdown-item"
-                                                to="/Productos/Accesorios"
-                                            >
-                                                Accesorios
-                                            </Link>
-                                        </li>
-                                        <li>
-                                            <Link
-                                                className="dropdown-item"
-                                                to="/Productos/Juegos De Mesa"
-                                            >
-                                                Juegos de Mesa
-                                            </Link>
-                                        </li>
-                                    </ul>
-                                </li>
-                            </ul>
-                            <li className="nav-item dropdown">
-                                <button
-                                    className="nav-link btn btn-nav btn-ripple-water"
-                                    aria-expanded="false"
-                                >
-                                    <i className="bi bi-person"></i> Cuenta
-                                </button>
-                                <ul
-                                    className="dropdown-menu"
-                                    aria-labelledby="navbarNavDropdown"
-                                >
-                                    <li>
-                                        <Link
-                                            className="dropdown-item"
-                                            to="/login"
-                                        >
-                                            Iniciar Sesión
-                                        </Link>
-                                    </li>
-                                    <li>
-                                        <Link
-                                            className="dropdown-item"
-                                            to="/register"
-                                        >
-                                            Registro
-                                        </Link>
-                                    </li>
-                                </ul>
-                            </li>
-                            <li>
-                                <Link
-                                    className="nav-link btn btn-nav btn-ripple-water"
-                                    to="/checkout"
-                                    aria-expanded="false"
-                                >
-                                    🛒 Carrito
-                                </Link>
-                            </li>
-                        </div>
-                    </div>
-                </div>
+        <span className="container fixed-top">
+            <div className="navbar">
+                <Link className="nav-icon" to="/">
+                    LevelUp
+                </Link>
+                <ul className="nav-items">
+                    <li>
+                        <Button linkClass="nav-link" to="/Productos">
+                            Productos
+                        </Button>
+                    </li>
+                    <li>
+                        <Button linkClass="nav-link" to="/Productos/TCG">
+                            Trading Card Games
+                        </Button>
+                    </li>
+                    <li>
+                        <Button linkClass="nav-link" to="/Productos/Accesorios">
+                            Accesorios
+                        </Button>
+                    </li>
+                    <li>
+                        <Button linkClass="nav-link" to="/login">
+                            Iniciar Sesion
+                        </Button>
+                    </li>
+                    <li>
+                        <Button linkClass="nav-link" to="/register">
+                            Registro
+                        </Button>
+                    </li>
+                    <li>
+                        <Button linkClass="nav-link" to="/Carrito">
+                            Carrito
+                        </Button>
+                    </li>
+                </ul>
             </div>
-        </>
+        </span>
     );
 }
 export default Navbar;
